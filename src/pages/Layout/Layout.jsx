6184d9facdba13a1e2913dc086c09666f09027bd
Vishnu <166791292+IncarnatorX@users.noreply.github.com>
2025-02-20 16:22:48 +0530

    login added

---
+++ src/pages/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router";
+import Header from "../../header/header";
+import Sidebar from "../../sidebar/Sidebar";
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="md:grid grid-cols-12 flex-grow h-0 flex-1 flex">
+        <div className="md:col-span-2">
+          <Sidebar />
+        </div>
+        <div className="md:col-span-10 flex-grow h-auto">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,32 +1,37 @@
 import { Route, Routes } from "react-router";
 
+import Layout from "../pages/Layout/Layout.jsx";
+
 // ADMIN DASHBOARD IMPORTS
 import AdminDashboard from "../pages/adminDashboard/AdminDashboard";
-import AdminLayout from "../pages/adminDashboard/AdminLayout";
+// import AdminLayout from "../pages/adminDashboard/AdminLayout";
 import Disputes from "../pages/adminDashboard/pages/Disputes/Disputes.jsx";
 import Employer from "../pages/adminDashboard/pages/Employer/Employer.jsx";
 import Monitoring from "../pages/adminDashboard/pages/Monitoring/Monitoring.jsx";
 
 // EMPLOYER DASHBOARD IMPORTS
-import EmployerLayout from "../pages/employerDashboard/EmployerLayout";
+// import EmployerLayout from "../pages/employerDashboard/EmployerLayout";
 import EmployerDashboard from "../pages/employerDashboard/EmployerDashboard";
 import Analytics from "../pages/employerDashboard/pages/Analytics/Analytics.jsx";
 import Blacklist from "../pages/employerDashboard/pages/Blacklist/Blacklist.jsx";
 import EmployeeList from "../pages/employerDashboard/pages/EmployeeList/EmployeeList.jsx";
 import History from "../pages/employerDashboard/pages/History/History.jsx";
 import Reviews from "../pages/employerDashboard/pages/Reviews/Reviews.jsx";
+import Login from "../components/Auth/Login/Login.jsx";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/admin" element={<AdminLayout />}>
+      <Route index path="/" element={<Login />} />
+
+      <Route path="/admin" element={<Layout />}>
         <Route index element={<AdminDashboard />} />
         <Route path="/admin/disputes" element={<Disputes />} />
         <Route path="/admin/employer" element={<Employer />} />
         <Route path="/admin/monitoring" element={<Monitoring />} />
       </Route>
 
-      <Route path="/company" element={<EmployerLayout />}>
+      <Route path="/company" element={<Layout />}>
         <Route index element={<EmployerDashboard />} />
         <Route path="/company/analytics" element={<Analytics />} />
         <Route path="/company/blacklist" element={<Blacklist />} />
